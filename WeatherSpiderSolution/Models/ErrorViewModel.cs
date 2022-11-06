a67793f2bd1b30919b30ec93e09871de04b4fa3a
SherozPuladov <sherozpulatov3@gmail.com>
2022-11-06 21:16:57 +0500

    Stable Version

---
+++ WeatherSpiderSolution/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace WeatherSpiderSolution.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
